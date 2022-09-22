5da13780dc7833e70ea8f535f2b80a5e57012984
xvost <konstantine.zam@yandex.ru>
2022-09-22 17:59:26 +0300

    Пофиксил верстку в функции get urls

--- olx.py
+++ olx.py
@@ -88,7 +88,7 @@ class OlxParser:
 
             soup = BeautifulSoup(content, 'lxml')
 
-            all_cards = soup.find_all('div', {'class': 'l-card'})
+            all_cards = soup.find_all('div', {'data-cy': 'l-card'})
             if len(all_cards) == 0:
                 break
 
